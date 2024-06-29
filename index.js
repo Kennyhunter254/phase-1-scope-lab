b012d2514588d9259207345798150408a3cc163e
Kennyhunter254 <kipronokevin566@gmail.com>
2024-06-29 15:34:39 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer in global scope
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Function to overwrite bestCustomer in global scope
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'; 
+}
+
+// Constant variable declared in global scope
+const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value'; 
+}
+
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  changeLeastFavoriteCustomer
+};
+
